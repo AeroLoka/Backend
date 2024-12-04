f8afaab61d5dbb72ec7101317663f1c40e3be69d
Andika Putra Wijaya <andika.pw2634@gmail.com>
2024-12-04 18:53:53 +0700

    feat: add register and login

--- routes/routes.js
+++ routes/routes.js
@@ -1,22 +1,31 @@
-const { createBooking, getAllBookingsByUserId } = require('../controllers/transaction-controller');
-const { getFlights } = require('../controllers/flightController');
+const {
+  createBooking,
+  getAllBookingsByUserId,
+} = require("../controllers/transaction-controller");
+const { getFlights } = require("../controllers/flightController");
 const {
   getAllFlights,
   getFlightById,
   createFlight,
   updateFlight,
   deleteFlight,
-} = require('../controllers/airfareControllers');
-const routes = require('express').Router();
+} = require("../controllers/airfareControllers");
 
-routes.post('/api/booking', createBooking);
-routes.get('/api/booking/:userId', getAllBookingsByUserId);
-routes.get('/api/search-flights', getFlights);
+const { register, login } = require("../controllers/authentication");
 
-routes.get('/api/flights/', getAllFlights);
-routes.get('/api/flights/:id', getFlightById);
-routes.post('/api/flights/', createFlight);
-routes.put('/api/flights/:id', updateFlight);
-routes.delete('/api/flights/:id', deleteFlight);
+const routes = require("express").Router();
+
+routes.post("/api/booking", createBooking);
+routes.get("/api/booking/:userId", getAllBookingsByUserId);
+routes.get("/api/search-flights", getFlights);
+
+routes.get("/api/flights/", getAllFlights);
+routes.get("/api/flights/:id", getFlightById);
+routes.post("/api/flights/", createFlight);
+routes.put("/api/flights/:id", updateFlight);
+routes.delete("/api/flights/:id", deleteFlight);
+
+routes.post("/api/register", register);
+routes.post("/api/login", login);
 
 module.exports = routes;
