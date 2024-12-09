f691d89851668dc3c5f42b38436614cecfdc4d9c
VincentAlfa <vincent111232@gmail.com>
2024-12-09 16:03:00 +0700

    fix: move url to env

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: `${process.env.CALLBACK_OAUTH}/google/callback`,
+      callbackURL: `/google/callback`,
       passReqToCallback: true,
     },
     async (request, accessToken, refreshToken, profile, done) => {
